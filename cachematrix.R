886b5ba3d5fb1c583f62b541b9560818a7272af7
kvvlamarroza <82015655+kvvlamarroza@users.noreply.github.com>
2021-04-15 15:39:41 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inversion
+## to solve a system of linear equation
 
-## Write a short comment describing this function
+## obtaining cache for matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+y <- NULL
+        put <- function (c) {
+                b <<- c
+                y <<- NULL
+}
+obtain <- function()b
+        putinverse <- function(inverse) y <<- inverse
+        obtaininverse <- function() y
+        list(put = put, obtain = obtain,
+             putinverse = putinverse
+             obtaininverse = obtaininverse)
 }
 
+## the code written above is use to calculate the data and sets of values for matrix inversion
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(b, ...) {
+        y <- b$obtaininverse()
+        if (!is.null(y)) {
+                message("preparing cached data")
+                return(y)
+}
+data <- b$obtain()
+        y <- solve(data, ...)
+        b$putinverse(y)
+        y
 }
